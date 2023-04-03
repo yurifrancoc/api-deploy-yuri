72f1a1ddab7e046581316df391193f9114a0f9b9
Yuri F <yurifrancoc@gmail.com>
2023-04-03 15:50:31 -0300

    dbheroku

--- src/conexao.js
+++ src/conexao.js
@@ -9,15 +9,18 @@
 //     port: 5432
 // });
 
+require('dotenv').config()
+
 const knex = require('knex')({
     client: 'pg',
     connection: {
-        host: 'localhost',
-        port: 5432,
-        user: 'postgres',
-        password: '123456',
-        database: 'market_cubos'
-    }
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_DATABASE
+    },
+    ssl: { rejectUnauthorized: false },
 });
 
 
